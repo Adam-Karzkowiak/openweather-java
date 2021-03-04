2bfe72628e23442587b61c4d02b34ebb13e8d40c
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-04 19:32:20 +0100

    method to change json into map added

--- src/main/java/OpenWeatherService.java
+++ src/main/java/OpenWeatherService.java
@@ -1,2 +1,18 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.HashMap;
+import java.util.Map;
+
 class OpenWeatherService {
+
+
+    public Map<String, Object> jsonToMap(String str) {
+        Map<String, Object> map = new Gson().fromJson(
+                str, new TypeToken<HashMap<String, Object>>() {
+                }.getType()
+        );
+        return map;
+    }
+
 }
