5d910dd59acc06a1b5a712ccd4dc1ac252bf7ccd
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-20 22:20:36 +0100

    Constants in dedicated class

---
+++ src/main/java/Constants.java
@@ -0,0 +1,8 @@
+public class Constants {
+    public static final String API_KEY = "[YOUR API KEY HERE";
+    public static final String LOCATION = "Wellington,NZ";
+    public static final String UNIT = "metric";
+    public static final String URL_ADDRESS = "http://api.openweathermap.org/data/2.5/weather?q="
+            + LOCATION + "&APPID=" + API_KEY + "&units=" + UNIT;
+
+}
